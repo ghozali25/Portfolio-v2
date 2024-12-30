ee454b544939b61e520d6f1ec71c235cc4d4069b
ghozali25 <ghozali.ahmd25@gmail.com>
2024-12-30 18:25:02 +0700

    commit

---
+++ src/components/footer/Footer.js
@@ -0,0 +1,21 @@
+import React from "react";
+import "./Footer.css";
+import { Fade } from "react-reveal";
+import { greeting } from "../../portfolio.js";
+/* eslint-disable jsx-a11y/accessible-emoji */
+
+export default function Footer(props) {
+  const currentYear = new Date().getFullYear(); // Mengambil tahun berjalan
+
+  return (
+    <div className="footer-div">
+      <Fade>
+        <p className="footer-text" style={{ color: props.theme.secondaryText }}>
+          {/*greeting.title8*/} 
+          © {currentYear} All rights reserved.
+        </p>
+        {/* <ToggleSwitch theme={props.theme} onToggle={props.onToggle}/> */}
+      </Fade>
+    </div>
+  );
+}
