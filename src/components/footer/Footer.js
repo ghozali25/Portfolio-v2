1fd0efa8702e275aa940a2ba21bf36109709591f
Ali <108579846+ghozali25@users.noreply.github.com>
2025-01-01 05:22:47 +0000

    update

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -1,8 +1,6 @@
 import React from "react";
 import "./Footer.css";
 import { Fade } from "react-reveal";
-import { greeting } from "../../portfolio.js";
-/* eslint-disable jsx-a11y/accessible-emoji */
 
 export default function Footer(props) {
   const currentYear = new Date().getFullYear(); // Mengambil tahun berjalan
@@ -11,8 +9,7 @@ export default function Footer(props) {
     <div className="footer-div">
       <Fade>
         <p className="footer-text" style={{ color: props.theme.secondaryText }}>
-          {/*greeting.title8*/} 
-          © {currentYear} All rights reserved.
+          {/*greeting.title8*/}© {currentYear} All rights reserved.
         </p>
         {/* <ToggleSwitch theme={props.theme} onToggle={props.onToggle}/> */}
       </Fade>
